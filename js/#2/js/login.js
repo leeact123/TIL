dbf31d9ae454f1db497d136c85ae74d3ce6ad530
leecode <lucky332533@gmail.com>
2022-12-25 13:07:18 +0900

    Update login.js

--- js/#2/js/login.js
+++ js/#2/js/login.js
@@ -11,17 +11,21 @@ document.querySelector("#modal").addEventListener("click", function () {
     toggle("show");
   })
 
-  const idInput = document.querySelector("#id-input");
-  const passInput = document.querySelector("#password-input");
-  
-  document.querySelector("form").addEventListener("submit", function(event){
-    event.preventDefault();
-    if ( idInput.value == '') {
-      alert("아이디입력하세요");
-    }
-    if ( passInput.value == '') {
-      alert("비번입력하세요");
-    }
-    if ( passInput.value.length < 6) {
-      alert("6자이상");
-    }});
+   //로그인창 안에 input
+   const idInput = document.querySelector("#id-input");
+   const passInput = document.querySelector("#password-input");
+   const loginForm = document.querySelector("#login-form")
+   
+   loginForm.addEventListener("submit", function(event){
+     event.preventDefault();
+     if ( idInput.value == '') {
+       alert("아이디입력하세요");
+     }
+     if (/\S+@\S+\.\S+/.test(idInput.value) == false) {
+       alert("이메일형식아님");
+     }
+     if ( /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/.test(passInput.value) == false) {
+       alert("비밀번호는 8자 이상이어야 하며, 숫자/대문자/소문자/특수문자를 모두 포함해야 합니다.");
+     }
+ 
+   });
