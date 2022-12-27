320f06c689e077905be6ca89fb6e78e808abdc27
leecode <lucky332533@gmail.com>
2022-12-27 21:30:10 +0900

    Update tap.js

--- js/#2/js/tap.js
+++ js/#2/js/tap.js
@@ -2,43 +2,59 @@ const tabBtn = document.querySelectorAll(".tab-button");
 const contentBox = document.querySelectorAll(".tab-content");
 
 
-
-function showSelectTab1() {
-    tabBtn.forEach(function(tab){
+    for (let i = 0; i < tabBtn.length; i++){
+    
+        tabBtn[i].addEventListener("click", function(){
+        tabBtn.forEach(function(tab){
         tab.classList.remove("selected");
-    });
-    tabBtn[0].classList.add("selected");
-    contentBox.forEach(function(content){
+        });
+        tabBtn[i].classList.add("selected");
+        contentBox.forEach(function(content){
         content.classList.remove("show");
+        });
+        contentBox[i].classList.add("show");
     })
-    contentBox[0].classList.add("show");
 }
 
-tabBtn[0].addEventListener("click", showSelectTab1);
 
-function showSelectTab2() {
-    tabBtn.forEach(function(tab){
-        tab.classList.remove("selected");
-    });
-    tabBtn[1].classList.add("selected");
-    contentBox.forEach(function(content){
-        content.classList.remove("show");
-    })
-    contentBox[1].classList.add("show");
-}
 
-tabBtn[1].addEventListener("click", showSelectTab2);
 
-function showSelectTab3() {
-    tabBtn.forEach(function(tab){
-        tab.classList.remove("selected");
-    });
-    tabBtn[2].classList.add("selected");
-    contentBox.forEach(function(content){
-        content.classList.remove("show");
-    })
-    contentBox[2].classList.add("show");
-}
+// function showSelectTab1() {
+//     tabBtn.forEach(function(tab){
+//         tab.classList.remove("selected");
+//     });
+//     tabBtn[0].classList.add("selected");
+//     contentBox.forEach(function(content){
+//         content.classList.remove("show");
+//     })
+//     contentBox[0].classList.add("show");
+// }
 
-tabBtn[2].addEventListener("click", showSelectTab3);
+// tabBtn[0].addEventListener("click", showSelectTab1);
+
+// function showSelectTab2() {
+//     tabBtn.forEach(function(tab){
+//         tab.classList.remove("selected");
+//     });
+//     tabBtn[1].classList.add("selected");
+//     contentBox.forEach(function(content){
+//         content.classList.remove("show");
+//     })
+//     contentBox[1].classList.add("show");
+// }
+
+// tabBtn[1].addEventListener("click", showSelectTab2);
+
+// function showSelectTab3() {
+//     tabBtn.forEach(function(tab){
+//         tab.classList.remove("selected");
+//     });
+//     tabBtn[2].classList.add("selected");
+//     contentBox.forEach(function(content){
+//         content.classList.remove("show");
+//     })
+//     contentBox[2].classList.add("show");
+// }
+
+// tabBtn[2].addEventListener("click", showSelectTab3);
 
