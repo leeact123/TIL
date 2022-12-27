f45c84c4828e7df84dddabbfec88eeac8308f720
leecode <lucky332533@gmail.com>
2022-12-27 20:05:26 +0900

    Create tap.js

---
+++ js/#2/js/tap.js
@@ -0,0 +1,44 @@
+const tabBtn = document.querySelectorAll(".tab-button");
+const contentBox = document.querySelectorAll(".tab-content");
+
+
+
+function showSelectTab1() {
+    tabBtn.forEach(function(tab){
+        tab.classList.remove("selected");
+    });
+    tabBtn[0].classList.add("selected");
+    contentBox.forEach(function(content){
+        content.classList.remove("show");
+    })
+    contentBox[0].classList.add("show");
+}
+
+tabBtn[0].addEventListener("click", showSelectTab1);
+
+function showSelectTab2() {
+    tabBtn.forEach(function(tab){
+        tab.classList.remove("selected");
+    });
+    tabBtn[1].classList.add("selected");
+    contentBox.forEach(function(content){
+        content.classList.remove("show");
+    })
+    contentBox[1].classList.add("show");
+}
+
+tabBtn[1].addEventListener("click", showSelectTab2);
+
+function showSelectTab3() {
+    tabBtn.forEach(function(tab){
+        tab.classList.remove("selected");
+    });
+    tabBtn[2].classList.add("selected");
+    contentBox.forEach(function(content){
+        content.classList.remove("show");
+    })
+    contentBox[2].classList.add("show");
+}
+
+tabBtn[2].addEventListener("click", showSelectTab3);
+
