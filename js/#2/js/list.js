a768956eb9f16a3e71a3bfcea75931101bba7f1e
leecode <lucky332533@gmail.com>
2022-12-29 23:37:51 +0900

    Create list.js

---
+++ js/#2/js/list.js
@@ -0,0 +1,17 @@
+var products = [
+    { id : 0, price : 70000, title : 'Blossom Dress' },
+    { id : 1, price : 50000, title : 'Springfield Shirt' },
+    { id : 2, price : 60000, title : 'Black Monastery' }
+  ];
+
+  const cardTitle = document.querySelectorAll(".card-body h5");
+  const cardPrice = document.querySelectorAll(".card-body p");
+
+  for(i=0; i < products.length; i++) {
+      
+    cardTitle[i].innerHTML = products[i].title;
+    cardPrice[i].innerHTML = products[i].price;
+
+  }
+
+  
